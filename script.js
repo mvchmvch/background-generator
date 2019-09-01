c441ddade8c898d6aa8b358f8be36233ce594ad7
Mikhail Chudakov <mvch@list.ru>
2019-09-01 15:31:29 +0300

    adding initial files to the project

---
+++ script.js
@@ -0,0 +1,40 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.getElementById("random");
+
+function changeBackground(){
+	body.style.background = 
+	  "linear-gradient(to right," +
+	  color1.value +
+	  ", " +
+	  color2.value +
+	  ")";
+
+	css.textContent = body.style.background+";";
+}
+
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+function randomBackground() {
+	color1.value = getRandomColor();
+	color2.value = getRandomColor();
+	changeBackground();
+}
+
+color1.addEventListener("input", changeBackground);
+color2.addEventListener("input", changeBackground);
+button.addEventListener("click", randomBackground);
+
+changeBackground();
+
